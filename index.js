2bc429efcf026f63d18dca6db43fedce4a55791f
David <Muchahi 853@gmail.com>
2024-04-21 21:07:02 +0300

    Finished lab

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(function (response) {
+    return response.json();
+  }).then(function (json) {
+    return renderBooks(json)
+  })
   
 }
 
@@ -15,3 +21,6 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+
